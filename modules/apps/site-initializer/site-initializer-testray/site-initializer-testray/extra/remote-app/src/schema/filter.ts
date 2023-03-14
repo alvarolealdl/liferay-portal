3709ec2620b7a9a673c91877d47a315469266888
Alvaro Leal <alvaro.leal@liferay.com>
2023-03-14 10:47:20 -0300

    LPS-178400 Create component name and translate

--- modules/apps/site-initializer/site-initializer-testray/site-initializer-testray/extra/remote-app/src/schema/filter.ts
+++ modules/apps/site-initializer/site-initializer-testray/site-initializer-testray/extra/remote-app/src/schema/filter.ts
@@ -535,6 +535,16 @@ const filterSchema = {
 			baseFilters.hasRequirements,
 		] as RendererFields[],
 	},
+	components: {
+		fields: [
+			{
+				label: i18n.translate('component-name'),
+				name: 'name',
+				operator: 'contains',
+				type: 'text',
+			},
+		] as RendererFields[],
+	},
 	requirementCases: {
 		fields: [
 			baseFilters.priority,
